1fa1ca298085ec5b47ed5c3c76785849fb7212d4
Jacek <ja77co@wp.pl>
2019-05-22 19:17:31 +0200

    dodatkowo z wykorzystaniem pętli wjile

--- src/WelcomeLoop.java
+++ src/WelcomeLoop.java
@@ -2,16 +2,40 @@ import java.util.Scanner;
 
 public class WelcomeLoop {
     public static void main(String[] args) {
-        String [] names = new String[5];
         Scanner scan = new Scanner(System.in);
-        int i = 0;
+
+        //użycie pętli "for"
+        String[] names = new String[5];
         System.out.println("Podaj pięć imion, po każdym użyj \"entera\":");
-        for (i = 0; i < names.length; i++){
+        for (int i = 0; i < names.length; i++) {
             names[i] = scan.nextLine();
         }
-
-        for(i = names.length-1; i >= 0; i--){
+        for (int i = names.length - 1; i >= 0; i--) {
             System.out.println("Cześć " + names[i]);
+        }
+
+        //użycie pętli "for" w wydaniu "for each"
+        String[] inna = new String[7];
+        for (int i = 0; i < inna.length; i++) {
+            inna[i] = scan.nextLine();
+            System.out.println("Wiataj" + inna[i] + " to jest i = " + i);
+        }
+        for (String info : inna) {
+            System.out.println("Cześć " + info);
+        }
+
+//użycie pętli "while"
+        String[] loopWhile = new String[5];
+        System.out.println("Podaj 5 imion: ");
+        int j = 0;
+        while (j < loopWhile.length) {
+            loopWhile[j] = scan.nextLine();
+            j++;
+        }
+        int k = loopWhile.length - 1;
+        while (k >= 0) {
+            System.out.println("Witam: " + loopWhile[k]);
+            k--;
         }
 
     }
